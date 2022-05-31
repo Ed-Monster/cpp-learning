9eae1c5b8855e37d31cf09f4842351b344cd7671
Ed-Monster <ed_zzf@163.com>
2022-05-31 14:53:03 +0800

    Lesson1 completed

---
+++ Lesson1/src/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void){
+    using std::cin;
+    using std::cout;
+    using std::endl;
+
+    cout << "Welcome to c++ world!" << endl;
+    cin.get();
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# cpp-learning
+- [Lesson1](Lesson1/README.md)
